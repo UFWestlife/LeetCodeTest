85399cab50b893da4296d3154dc0c73478988750
ufwestlife <uflzun93@gmail.com>
2017-10-10 15:46:13 -0400

    new work 10.10.2017

--- RandomizedSet380/RandomizedSet.java
+++ RandomizedSet380/RandomizedSet.java
@@ -14,8 +14,7 @@ public class RandomizedSet {
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        boolean contain = locs.containsKey(val);
-        if ( contain ) return false;
+        if ( locs.containsKey(val) ) return false;
         locs.put( val, nums.size());
         nums.add(val);
         return true;
@@ -24,8 +23,7 @@ public class RandomizedSet {
 
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        boolean contain = locs.containsKey(val);
-        if ( ! contain ) return false;
+        if ( !locs.containsKey(val) ) return false;
         int loc = locs.get(val);
         if (loc < nums.size() - 1 ) { // not the last one than swap the last one with this val
             int lastone = nums.get(nums.size() - 1 );
