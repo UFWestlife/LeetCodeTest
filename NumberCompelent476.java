a9e12af25cd455210a89832be3b9984652b526fe
Zun <uflzun93@gmail.com>
2018-01-14 10:18:14 -0500

    new work Jan.14.2018

---
+++ NumberCompelent476.java
@@ -0,0 +1,9 @@
+public class NumberCompelent476 {
+    public static int findComplement(int num) {
+        return 0;
+    }
+
+    public static void main(String args[]){
+        findComplement(5);
+    }
+}
